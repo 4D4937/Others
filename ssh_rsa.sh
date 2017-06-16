645141fe04ef782f77c18fb22915637056167004
Sao! <o_u1000@dragons.hutchcc.edu>
2017-06-16 18:32:29 +0800

    Update ssh_rsa.sh

--- ssh_rsa.sh
+++ ssh_rsa.sh
@@ -6,7 +6,7 @@ config_file='/etc/ssh/sshd_config'
 #Check Root
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
 
-# ssh_config
+#ssh_config
 cd /root/.ssh/
 wget https://github.com/4D4937/Others/edit/master/ssh_key
 cat /root/.ssh/libertyss_rsa.pub >>  /root/.ssh/authorized_keys
@@ -14,6 +14,7 @@ sed -i "47s/#/ /g" ${config_file}
 sed -i "48s/#/ /g" ${config_file}
 sed -i "49s/#/ /g" ${config_file} 
 sed -i "64s/yes/no/g" ${config_file}
+
 service sshd restart
 
 echo done!
