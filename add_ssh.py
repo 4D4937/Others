0187cc1d1d21ce32caebf6f6d65c23d1eba9f7ae
Dr. Z <o_u1000@dragons.hutchcc.edu>
2020-10-25 12:02:09 +0800

    Update add_ssh.py

--- add_ssh.py
+++ add_ssh.py
@@ -2,10 +2,10 @@
 import os
 
 os.system("apt-get install expect -y")
-node_name = raw_input("Enter node name")
-user_name = raw_input('Enter user name')
-ip_address = raw_input('Enter ip address')
-password = raw_input('Enter password')
+node_name = raw_input("Enter node name：")
+user_name = raw_input('Enter user name：')
+ip_address = raw_input('Enter ip address：')
+password = raw_input('Enter password：')
 
 f = open('/home/zrhe2016/' + str(node_name) + '.sh','w')
 f.write('#!/usr/bin/expect\n' + 'spawn ssh ' + str(user_name) + '@' + str(ip_address) + '\n' + 'expect "*password:"\n' + 'send "' + str(password) + '\\' + 'r"\n' + 'interact\n')
