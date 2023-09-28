e083e833fe9c24e13b4b54420e3e0bee16428782
zrhe2016 <zrhe2016@gmail.com>
2023-09-28 20:58:14 +0800

    Update aes_crypto_tool.py

--- aes_crypto_tool.py
+++ aes_crypto_tool.py
@@ -1,45 +1,59 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from base64 import b64encode, b64decode
+from cryptography.hazmat.primitives import padding
+from getpass import getpass
+
 
 def pad(text):
-    block_size = 16
-    padding = block_size - (len(text) % block_size)
-    return text + bytes([padding] * padding)
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(text) + padder.finalize()
+    return padded_data
 
-def unpad(text):
-    padding = text[-1]
-    return text[:-padding]
 
-def encrypt_ecb(text, key):
-    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
+def unpad(padded_data):
+    unpadder = padding.PKCS7(128).unpadder()
+    data = unpadder.update(padded_data) + unpadder.finalize()
+    return data
+
+
+def encrypt_cbc(text, key, iv):
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor()
     padded_text = pad(text)
     ciphertext = encryptor.update(padded_text) + encryptor.finalize()
     return b64encode(ciphertext)
 
-def decrypt_ecb(ciphertext, key):
-    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
+
+def decrypt_cbc(ciphertext, key, iv):
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     ciphertext = b64decode(ciphertext)
     padded_text = decryptor.update(ciphertext) + decryptor.finalize()
     return unpad(padded_text)
 
+
 def main():
-    key = input("Enter the key: ").encode('utf-8')
+    key = getpass("Enter the key: ").encode('utf-8')
+    iv = getpass("Enter the IV: ").encode('utf-8')
+
+    # Pad key and IV if their length is less than 16 bytes
+    key = key.ljust(16, b'\x00')
+    iv = iv.ljust(16, b'\x00')
 
     operation = input("Enter 'e' for encryption or 'd' for decryption: ")
 
     if operation == 'e':
         text = input("Enter the text to encrypt: ").encode('utf-8')
-        encrypted_text = encrypt_ecb(text, key)
-        print("Encrypted Text:", encrypted_text)
+        encrypted_text = encrypt_cbc(text, key, iv)
+        print("Encrypted Text:", encrypted_text.decode('utf-8'))
     elif operation == 'd':
         text = input("Enter the text to decrypt (in base64 format): ")
-        decrypted_text = decrypt_ecb(text, key)
+        decrypted_text = decrypt_cbc(text, key, iv)
         print("Decrypted Text:", decrypted_text.decode('utf-8'))
     else:
         print("Invalid operation. Please enter 'e' or 'd'.")
+
 
 if __name__ == "__main__":
     main()
