77b0c1bb1de5e838a8bf8fa372ab07c50a2b5067
zrhe2016 <zrhe2016@gmail.com>
2023-09-30 00:41:02 +0800

    Update BTCPriceDifferenceCalculator.py

--- BTCPriceDifferenceCalculator.py
+++ BTCPriceDifferenceCalculator.py
@@ -12,26 +12,32 @@ def get_btc_cny_price():
         return None
 
 def calculate_investment_value(initial_investment, first_btc_price, second_btc_price, current_btc_price):
-    first_btc_quantity = initial_investment / first_btc_price
-    second_btc_quantity = initial_investment / second_btc_price
-    btc_quantity_difference = abs(first_btc_quantity - second_btc_quantity)
-    investment_value = btc_quantity_difference * current_btc_price
-    
-    # Print the formula results
-    print("First BTC quantity:", first_btc_quantity)
-    print("Second BTC quantity:", second_btc_quantity)
-    print("BTC quantity difference:", btc_quantity_difference)
-    print("BTC price difference:", investment_value)
-    
-    return investment_value
+    btc_cny_price = get_btc_cny_price()
+    if btc_cny_price is not None:
+        first_btc_quantity = initial_investment / first_btc_price
+        second_btc_quantity = initial_investment / second_btc_price
+        first_btc_value = first_btc_quantity * btc_cny_price
+        second_btc_value = second_btc_quantity * btc_cny_price
+        value_difference = abs(first_btc_value - second_btc_value)
+        first_btc_loss = round(((initial_investment - first_btc_value) / initial_investment) * 100, 2)
+        second_btc_loss = round(((initial_investment - second_btc_value) / initial_investment) * 100, 2)
+
+        # Print the formula results
+        print("First BTC value:", round(first_btc_value, 2))
+        print("Second BTC value:", round(second_btc_value, 2))
+        print("Value difference:", round(value_difference, 2))
+        print("First BTC loss:", first_btc_loss, "%")
+        print("Second BTC loss:", second_btc_loss, "%")
+
+        return value_difference
 
 # Get input values from the user
 initial_investment = float(input("Enter the initial investment amount: "))
 first_btc_price = float(input("Enter the price of the first BTC: "))
 second_btc_price = float(input("Enter the price of the second BTC: "))
+current_btc_price = get_btc_cny_price()
 
-btc_cny_price = get_btc_cny_price()
-if btc_cny_price is not None:
-    result = calculate_investment_value(initial_investment, first_btc_price, second_btc_price, btc_cny_price)
+if current_btc_price is not None:
+    result = calculate_investment_value(initial_investment, first_btc_price, second_btc_price, current_btc_price)
 
 input("Press Enter to exit the program")
