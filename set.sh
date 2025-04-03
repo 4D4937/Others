1172e4dc24f25781f424a394bab3d709f351446c
zrhe2016 <zrhe2016@gmail.com>
2025-04-03 14:30:13 +0800

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 更新系统并安装必要的软件包
-yum -y install network-scripts lrzsz tree net-tools ntpdate wget zip
-
 # 配置 YUM 源
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.cloud.tencent.com/repo/centos7_base.repo
 
@@ -10,6 +7,10 @@ yum clean all
 yum makecache
 
 echo "YUM 源配置完成"
+
+# 更新系统并安装必要的软件包
+yum -y update
+yum -y install network-scripts lrzsz tree net-tools ntpdate wget zip
 
 # 关闭防火墙
 systemctl stop firewalld
