40d804e7c692a9ea9ba67b7f311c202cca4975ad
zrhe2016 <zrhe2016@gmail.com>
2025-04-04 19:22:30 +0800

    Update zerotier.sh

--- zerotier.sh
+++ zerotier.sh
@@ -7,10 +7,16 @@ curl -s https://install.zerotier.com | sudo bash
 sudo systemctl enable zerotier-one
 sudo systemctl start zerotier-one
 
+# 使用 orbit 设置节点
+IDENTITY="b88b96282a"
+sudo zerotier-cli orbit $IDENTITY $IDENTITY
+
 # 加入指定的 ZeroTier 网络
 NETWORK_ID="6ab565387aed6259"
 sudo zerotier-cli join $NETWORK_ID
 
-# 检查是否成功加入网络
+# 等待网络连接初始化
 sleep 5
+
+# 检查是否成功加入网络
 sudo zerotier-cli listnetworks
