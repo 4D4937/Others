042da7c745ece5ce9f55de9c2ade144d964256ec
zrhe2016 <zrhe2016@gmail.com>
2025-04-04 19:59:21 +0800

    Update set.sh

--- set.sh
+++ set.sh
@@ -4,13 +4,13 @@
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 
 yum clean all
-#yum makecache
+yum makecache
 
 echo "YUM 源配置完成"
 
 # 更新系统并安装必要的软件包
-#yum -y update
-#yum -y install network-scripts lrzsz tree net-tools ntpdate wget zip
+yum -y update
+yum -y install network-scripts lrzsz tree net-tools ntpdate wget zip
 
 # 关闭防火墙
 systemctl stop firewalld
