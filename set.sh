d410407ec02f8e61d106d43c4c56f3b08d76d910
zrhe2016 <zrhe2016@gmail.com>
2025-04-12 16:05:42 +0800

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 配置 YUM 源
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 
 yum clean all
 yum makecache
@@ -18,9 +18,9 @@ systemctl disable firewalld
 echo "防火墙已关闭"
 
 # 关闭 SELinux
-sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-setenforce 0
-echo "SELinux 已关闭"
+# sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+# setenforce 0
+# echo "SELinux 已关闭"
 
 # 配置定时同步时间
 echo "*/15 * * * * root /usr/sbin/ntpdate ntp1.aliyun.com &>/dev/null" >> /etc/crontab
@@ -28,4 +28,4 @@ systemctl restart crond
 echo "时间同步任务已配置"
 
 # 重启服务器
-reboot
+# reboot
