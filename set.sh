1ef69efd738f718e367281908724507216a980ac
zrhe2016 <zrhe2016@gmail.com>
2025-04-12 16:10:48 +0800

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 配置 YUM 源
-curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS7-Base-163.repo
 
 yum clean all
 yum makecache
