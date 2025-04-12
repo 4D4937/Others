e89bb2127aaf1b5bb54458fe682a9313f9fe65b0
zrhe2016 <zrhe2016@gmail.com>
2025-04-12 19:17:39 +0800

    Update set.sh

--- set.sh
+++ set.sh
@@ -9,14 +9,14 @@ yum -y update
 yum -y install network-scripts lrzsz tree net-tools ntpdate wget zip
 
 # 关闭防火墙
-# systemctl stop firewalld
-# systemctl disable firewalld
-# echo "防火墙已关闭"
+systemctl stop firewalld
+systemctl disable firewalld
+echo "防火墙已关闭"
 
 # 关闭 SELinux
-# sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-# setenforce 0
-# echo "SELinux 已关闭"
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+setenforce 0
+echo "SELinux 已关闭"
 
 # 配置定时同步时间
 echo "*/15 * * * * root /usr/sbin/ntpdate ntp1.aliyun.com &>/dev/null" >> /etc/crontab
@@ -24,4 +24,4 @@ systemctl restart crond
 echo "时间同步任务已配置"
 
 # 重启服务器
-# reboot
+reboot
