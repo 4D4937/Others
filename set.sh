ab70102c2273582df4b65799f59edd48e73c80e3
zrhe2016 <zrhe2016@gmail.com>
2025-04-16 21:10:39 +0800

    Update set.sh

--- set.sh
+++ set.sh
@@ -20,6 +20,10 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 setenforce 0
 echo "SELinux 已关闭"
 
+# disable automatic updates
+systemctl stop packagekit.service
+systemctl mask packagekit.service
+
 # 配置定时同步时间
 timedatectl set-timezone Asia/Shanghai 
 ntpdate cn.pool.ntp.org
